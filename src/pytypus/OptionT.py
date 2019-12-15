5c0cda56868866a28e32ab86f7ba2cc204c0f78e
Samuele Reghenzi <sam@reghenzi.cloud>
2019-12-15 22:27:48 +0100

    more experiments

--- src/pytypus/OptionT.py
+++ src/pytypus/OptionT.py
@@ -4,14 +4,14 @@ from typing import Generic, TypeVar, Union, Callable
 T = TypeVar('T')
 S = TypeVar('S')
 
-class OptionT(Generic[T,S]):
+class OptionT(Generic[T]):
 
     value : Union[T, None] 
 
     def __init__(self,v: Union[T, None]) -> None:
         self.value = v
 
-    def map(self, f:Callable[[T],S]) -> OptionT[S]:
+    def map(self, f:Callable[[T],S]) -> Union[S, None]:
         if(self.value):
             return  OptionT(f(self.value))
         else:   
