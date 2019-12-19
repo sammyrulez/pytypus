6b2626affc3bfa023f4562bde794fe391cc486ee
Samuele Reghenzi <sam@reghenzi.cloud>
2019-12-19 18:46:00 +0100

    option tests

--- pytypus/option.py
+++ pytypus/option.py
@@ -1,30 +1,73 @@
 from __future__ import annotations
-from typing import Generic, TypeVar, Union, Callable
+from pytypus.functor import Functor
+from typing import Generic, Iterable, TypeVar, Union, Callable
 
 
 T = TypeVar('T')
 S = TypeVar('S')
 
 
-class OptionT(Generic[T]):
+class OptionT(Functor[T]):
 
     def __init__(self, v: Union[T, None]) -> None:
-        self.value = v
+        self._value = v
 
     def map(self, f: Callable[[T], S]) -> OptionT[S]:
-        if(self.value):
-            return OptionT(f(self.value))
+        if(self._value):
+            return OptionT(f(self._value))
         else:
             return OptionT(None)
 
-    def flatMap(self, f: Callable[[T], OptionT[S]]) -> OptionT[S]:
-        if(self.value):
-            return f(self.value)
+    def flat_map(self, f: Callable[[T], OptionT[S]]) -> OptionT[S]:
+        if(self._value):
+            return f(self._value)
         else:
             return OptionT(None)
 
     def is_empty(self):
-        return self.value is None
+        return self._value is None
+
+    def or_value(self, v: T) -> T:
+        if(self._value):
+            return self._value
+        else:
+            return v
+
+    def or_rise(self, e: RuntimeError) -> T:
+        if(self._value):
+            return self._value
+        else:
+            raise e
+
+    def or_else(self, v: T) -> OptionT[T]:
+        if(self._value):
+            return self
+        else:
+            return OptionT(v)
+
+    def __str__(self) -> str:
+        if(self._value):
+            return str(self._value)
+        else:
+            return "EmptyOption"
+
+    def __eq__(self, o: object) -> bool:
+        if(self._value):
+            return self._value == o
+        else:
+            return False
+
+    def __sizeof__(self) -> int:
+        if(self._value):
+            return 1
+        else:
+            return 0
+
+    def __iter__(self) -> Iterable[T]:
+        if(self._value):
+            return iter(self._value)
+        else:
+            return iter([])
 
 
 def empty() -> OptionT[any]:
--- tests/test_option.py
+++ tests/test_option.py
@@ -1,20 +1,29 @@
 from __future__ import annotations
 from typing import Any, Callable, Generic, TypeVar
-
+from pytypus import option
 T = TypeVar("T")
 S = TypeVar("S")
 
 
-class Functor(Generic[T]):
-    def map(self, function: Callable[[T], S]) -> Functor[S]:
-        raise NotImplementedError
-
-
 def test_map():
-    from pytypus import option
+
     e = option.empty()
     mapped_e = e.map(lambda k: str(k))
     assert mapped_e.is_empty()
     f = option.some("string")
     mapped_f = f.map(lambda s: len(s))
-    assert mapped_f.value == 6
+    assert mapped_f.or_value(5) == 6
+
+
+def test_obj_protocols():
+    counter = 0
+    e = option.empty()
+    assert str(e) == "EmptyOption"
+    for x in e:
+        counter = counter + 1
+    assert counter == 0
+    f = option.some("string")
+    assert str(f) == "string"
+    for x in f:
+        counter = counter + 1
+    assert counter > 0
