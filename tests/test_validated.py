1f409bcf199d3b8971491ec58b6d12b5dd8be36c
Samuele Reghenzi <sam@reghenzi.cloud>
2020-02-17 22:00:25 +0100

    regexp

--- tests/test_validated.py
+++ tests/test_validated.py
@@ -2,6 +2,7 @@ from pytypus.either import Either, cond
 from pytypus.validated import Valid, Validated, from_Either
 from pytypus.list import Chain
 from dataclasses import dataclass
+import re
 
 
 @dataclass
@@ -29,19 +30,13 @@ class UsernameHasSpecialCharacters(DomainValidation):
 class RegistrationFormValidator(object):
 
     def validateUsername(self, username: str) -> Validated[DomainValidation, str]:
-        return from_Either(cond(True, username, UsernameHasSpecialCharacters))
+        match = re.findall(r"(?:^|(?<=\s))", username, flags=re.IGNORECASE)
+        return from_Either(cond(match, username, UsernameHasSpecialCharacters))
 
     def validateForm(self, username: str, password: str, first_name: str, last_name: str, age: int) -> Validated[Chain[DomainValidation], RegistrationData]:
-        """  for { 
-            validatedUserName <- validateUserName(username)
-            validatedPassword <- validatePassword(password)
-            validatedFirstName <- validateFirstName(firstName)
-            validatedLastName <- validateLastName(lastName)
-            validatedAge <- validateAge(age)
-        } yield RegistrationData(validatedUserName, validatedPassword, validatedFirstName, validatedLastName, validatedAge)
-        """
-        validationResult = Chain([self.validateUsername(
-            username), password, first_name, last_name, age])
+        validationResult = Chain([
+            self.validateUsername(username),
+            password, first_name, last_name, age])
 
         return from_Either(cond(validationResult.filter(lambda e: isinstance(e, Valid)), validationResult, RegistrationData(username, password, first_name, last_name, age)))
 
